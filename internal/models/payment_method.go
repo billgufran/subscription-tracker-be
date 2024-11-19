710058d57ec37cf1aa5b4bf0adbf9ccc83949e0b
billgufran <billgufran@gmail.com>
2024-11-19 21:40:02 +0700

    update payment method type's type

--- internal/models/payment_method.go
+++ internal/models/payment_method.go
@@ -6,13 +6,22 @@ import (
 	"gorm.io/gorm"
 )
 
+type PaymentMethodType string
+
+const (
+	PaymentMethodTypeCreditCard    PaymentMethodType = "credit_card"
+	PaymentMethodTypeDebitCard     PaymentMethodType = "debit_card"
+	PaymentMethodTypeBankAccount   PaymentMethodType = "bank_account"
+	PaymentMethodTypeDigitalWallet PaymentMethodType = "digital_wallet"
+)
+
 type PaymentMethod struct {
-	ID        uint   `gorm:"primaryKey"`
-	UserID    uint   `gorm:"not null"`
-	User      User   `gorm:"foreignKey:UserID"`
-	Name      string `gorm:"not null"`
-	Type      string `gorm:"not null"`
-	LastFour  string `gorm:"type:varchar(4)"`
+	ID        uint              `gorm:"primaryKey"`
+	UserID    uint              `gorm:"not null"`
+	User      User              `gorm:"foreignKey:UserID"`
+	Name      string            `gorm:"not null"`
+	Type      PaymentMethodType `gorm:"not null;type:varchar(20)"`
+	LastFour  string            `gorm:"type:varchar(4)"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
